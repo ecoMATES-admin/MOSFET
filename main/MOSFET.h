2ba33978f615e8d32ea1a88ce1dbb9eb3505b642
eric bochert <e.bochert@eco-mates.de>
2021-09-21 15:47:02 +0200

    HF PWM added

--- main/MOSFET.h
+++ main/MOSFET.h
@@ -1,20 +1,30 @@
 #ifndef MOSFET_H
 #define MOSFET_H
 
-
+// High Frequncy Mode wird nur auf Pin 9 und 10 unterstützt, denn nur Timer 1 wird verändert
 class MOSFET{
   private:
     uint8_t _gate;
+    bool _hfPwm;
+    const byte _mask = B11111000;
+    int _prescale = 2;
 
   public:
-    MOSFET(uint8_t gate): _gate(gate)
-    {};
+    MOSFET(uint8_t gate, bool hfPwm): _gate(gate), _hfPwm(hfPwm)
+    {
+      if(_hfPwm){
+        TCCR1B = (TCCR1B & _mask)| _prescale;
+      }
+    };
     void on(){
       analogWrite(_gate, 255);
     };
     void off(){
       analogWrite(_gate, 0);
-    };  
+    };
+    void pwm(uint8_t dc){
+      analogWrite(_gate, dc); 
+    }
 };
 
 
--- main/main.ino
+++ main/main.ino
@@ -1,1 +1,15 @@
 
+void setup(){
+  
+}
+
+void loop(){
+  //Rampe
+  /*
+   * überprüfe Lüfter Datasheet auf was für eine Spannung sie maximal gedrosselt werden können
+   * Rampe ohne HF PWM
+   * Rampe mit HF PWM
+   * Vergleich
+   * 
+   */
+}
